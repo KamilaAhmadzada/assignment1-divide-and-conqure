bae577d7e47f49a12112475a4d97cbd3e7b36f90
Kamila Ahmadzada <255888@astanait.kz.edu>
2026-09-20 01:32:13 +0500

    added Point class

---
+++ src/main/java/com/assignment1/Point.java
@@ -0,0 +1,17 @@
+package com.assignment1;
+
+public class Point {
+    public final double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point other) {
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+}
